d947f8f94997bf785f046b2c1ae4c09ea37efa10
Canka76 <karakoccan04@gmail.com>
2025-05-06 16:11:47 +0300

    double hashing tests succeed

---
+++ tests/DoubleHashingTests.cs
@@ -0,0 +1,19 @@
+﻿public class DoubleHashingHashTableTests
+{
+    [Fact]
+    public void Insert_And_Search_ShouldReturnTrue()
+    {
+        var hashTable = new DoubleHashingHashTable(10);
+        hashTable.Insert(35);
+        Assert.True(hashTable.Search(35));
+    }
+
+    [Fact]
+    public void Delete_ShouldRemoveItem()
+    {
+        var hashTable = new DoubleHashingHashTable(10);
+        hashTable.Insert(35);
+        hashTable.Delete(35);
+        Assert.False(hashTable.Search(35));
+    }
+}
