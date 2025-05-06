f7dce50559ae6c7042951c9026d7e857546a5f8a
Canka76 <karakoccan04@gmail.com>
2025-05-06 15:51:26 +0300

    seperate chaining test

---
+++ tests/SeperateChainingTests.cs
@@ -0,0 +1,19 @@
+﻿public class SeparateChainingHashTableTests
+{
+    [Fact]
+    public void Insert_And_Search_ShouldReturnTrue()
+    {
+        var hashTable = new SeparateChainingHashTable(5);
+        hashTable.Insert(15);
+        Assert.True(hashTable.Search(15));
+    }
+
+    [Fact]
+    public void Delete_ShouldRemoveItem()
+    {
+        var hashTable = new SeparateChainingHashTable(5);
+        hashTable.Insert(15);
+        hashTable.Delete(15);
+        Assert.False(hashTable.Search(15));
+    }
+}
