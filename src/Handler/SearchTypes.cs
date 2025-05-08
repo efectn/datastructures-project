6292aef0ee8e8dad47e86e0545e3123271eef166
Muhsin4155 <muhsin4155@icloud.com>
2025-05-09 02:01:23 +0300

    Update SearchTypes.cs

--- src/Handler/SearchTypes.cs
+++ src/Handler/SearchTypes.cs
@@ -1,1 +1,5 @@
-using System;
+using datastructures_project.Document;
+using datastructures_project.Dto;
+using datastructures_project.HashTables;
+using datastructures_project.Search.Score;
+using datastructures_project.Search.Tokenizer;
