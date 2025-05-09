b9c818beeace1f9c250588dc3097d2acf860e6ed
schzoid <fhalucard@gmail.com>
2025-05-10 00:15:51 +0300

    fully implemented IDictionary

--- tests/BTreeSkeleton.cs
+++ tests/BTreeSkeleton.cs
@@ -35,6 +35,11 @@ public class BTreeDictionary<TKey, TValue> : IDictionary<TKey, TValue> where TKe
         }
 
         InsertNonFull(_root, key, value);
+        Count++;
+    }
+    void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
+    {
+        Add(item.Key, item.Value);
     }
 
     private void InsertNonFull(Node node, TKey key, TValue value)
@@ -125,21 +130,229 @@ public class BTreeDictionary<TKey, TValue> : IDictionary<TKey, TValue> where TKe
                 return val;
             throw new KeyNotFoundException();
         }
-        set => Add(key, value); // Overwrites existing keys (you can change this to prevent duplicates)
+        set
+        {
+            Remove(key);
+            Add(key, value);
+        }
     }
 
-    #region Not Fully Implemented IDictionary Members
-    public ICollection<TKey> Keys => throw new NotImplementedException();
-    public ICollection<TValue> Values => throw new NotImplementedException();
-    public int Count => throw new NotImplementedException();
+    public ICollection<TKey> Keys
+    {
+        get
+        {
+            var keys = new List<TKey>();
+            Traverse(_root, (k, _) => keys.Add(k));
+            return keys;
+        }
+    }
+
+    public ICollection<TValue> Values
+    {
+        get
+        {
+            var values = new List<TValue>();
+            Traverse(_root, (_, v) => values.Add(v));
+            return values;
+        }
+    }
+
+    public int Count { get; private set; } = 0;
+
     public bool IsReadOnly => false;
-    public void Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
-    public void Clear() => throw new NotImplementedException();
-    public bool Remove(TKey key) => throw new NotImplementedException();
-    public bool Remove(KeyValuePair<TKey, TValue> item) => throw new NotImplementedException();
-    public bool Contains(KeyValuePair<TKey, TValue> item) => throw new NotImplementedException();
-    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => throw new NotImplementedException();
-    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => throw new NotImplementedException();
+
+    public void Clear()
+    {
+        _root = new Node();
+        Count = 0;
+    }
+
+    public bool Remove(TKey key)
+    {
+        if (!ContainsKey(key)) return false;
+
+        Remove(_root, key);
+        if (_root.Keys.Count == 0 && !_root.IsLeaf)
+            _root = _root.Children[0];
+
+        Count--;
+        return true;
+    }
+
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        if (Contains(item))
+        {
+            return Remove(item.Key);
+        }
+        return false;
+    }
+
+    private void Remove(Node node, TKey key)
+    {
+        int idx = node.Keys.FindIndex(k => k.CompareTo(key) >= 0);
+
+        if (idx >= 0 && idx < node.Keys.Count && node.Keys[idx].CompareTo(key) == 0)
+        {
+            if (node.IsLeaf)
+            {
+                node.Keys.RemoveAt(idx);
+                node.Values.RemoveAt(idx);
+            }
+            else
+            {
+                if (node.Children[idx].Keys.Count >= _t)
+                {
+                    (TKey predKey, TValue predVal) = GetPredecessor(node.Children[idx]);
+                    node.Keys[idx] = predKey;
+                    node.Values[idx] = predVal;
+                    Remove(node.Children[idx], predKey);
+                }
+                else if (node.Children[idx + 1].Keys.Count >= _t)
+                {
+                    (TKey succKey, TValue succVal) = GetSuccessor(node.Children[idx + 1]);
+                    node.Keys[idx] = succKey;
+                    node.Values[idx] = succVal;
+                    Remove(node.Children[idx + 1], succKey);
+                }
+                else
+                {
+                    Merge(node, idx);
+                    Remove(node.Children[idx], key);
+                }
+            }
+        }
+        else if (!node.IsLeaf)
+        {
+            if (idx < 0) idx = node.Keys.Count;
+            if (node.Children[idx].Keys.Count < _t)
+                Fill(node, idx);
+            Remove(node.Children[idx], key);
+        }
+    }
+
+    private void Fill(Node node, int idx)
+    {
+        if (idx > 0 && node.Children[idx - 1].Keys.Count >= _t)
+            BorrowFromPrev(node, idx);
+        else if (idx < node.Children.Count - 1 && node.Children[idx + 1].Keys.Count >= _t)
+            BorrowFromNext(node, idx);
+        else
+        {
+            if (idx < node.Children.Count - 1)
+                Merge(node, idx);
+            else
+                Merge(node, idx - 1);
+        }
+    }
+
+    private void BorrowFromPrev(Node node, int idx)
+    {
+        var child = node.Children[idx];
+        var sibling = node.Children[idx - 1];
+
+        child.Keys.Insert(0, node.Keys[idx - 1]);
+        child.Values.Insert(0, node.Values[idx - 1]);
+
+        if (!child.IsLeaf)
+            child.Children.Insert(0, sibling.Children[^1]);
+
+        node.Keys[idx - 1] = sibling.Keys[^1];
+        node.Values[idx - 1] = sibling.Values[^1];
+
+        sibling.Keys.RemoveAt(sibling.Keys.Count - 1);
+        sibling.Values.RemoveAt(sibling.Values.Count - 1);
+        if (!sibling.IsLeaf)
+            sibling.Children.RemoveAt(sibling.Children.Count - 1);
+    }
+
+    private void BorrowFromNext(Node node, int idx)
+    {
+        var child = node.Children[idx];
+        var sibling = node.Children[idx + 1];
+
+        child.Keys.Add(node.Keys[idx]);
+        child.Values.Add(node.Values[idx]);
+
+        if (!child.IsLeaf)
+            child.Children.Add(sibling.Children[0]);
+
+        node.Keys[idx] = sibling.Keys[0];
+        node.Values[idx] = sibling.Values[0];
+
+        sibling.Keys.RemoveAt(0);
+        sibling.Values.RemoveAt(0);
+        if (!sibling.IsLeaf)
+            sibling.Children.RemoveAt(0);
+    }
+
+    private void Merge(Node node, int idx)
+    {
+        var child = node.Children[idx];
+        var sibling = node.Children[idx + 1];
+
+        child.Keys.Add(node.Keys[idx]);
+        child.Values.Add(node.Values[idx]);
+
+        child.Keys.AddRange(sibling.Keys);
+        child.Values.AddRange(sibling.Values);
+
+        if (!child.IsLeaf)
+            child.Children.AddRange(sibling.Children);
+
+        node.Keys.RemoveAt(idx);
+        node.Values.RemoveAt(idx);
+        node.Children.RemoveAt(idx + 1);
+    }
+
+    private (TKey, TValue) GetPredecessor(Node node)
+    {
+        while (!node.IsLeaf)
+            node = node.Children[^1];
+        return (node.Keys[^1], node.Values[^1]);
+    }
+
+    private (TKey, TValue) GetSuccessor(Node node)
+    {
+        while (!node.IsLeaf)
+            node = node.Children[0];
+        return (node.Keys[0], node.Values[0]);
+    }
+
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        if (TryGetValue(item.Key, out var val))
+            return EqualityComparer<TValue>.Default.Equals(val, item.Value);
+        return false;
+    }
+
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        foreach (var kvp in this)
+        {
+            array[arrayIndex++] = kvp;
+        }
+    }
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        var list = new List<KeyValuePair<TKey, TValue>>();
+        Traverse(_root, (k, v) => list.Add(new KeyValuePair<TKey, TValue>(k, v)));
+        return list.GetEnumerator();
+    }
+
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    #endregion
+
+    private void Traverse(Node node, Action<TKey, TValue> action)
+    {
+        int i;
+        for (i = 0; i < node.Keys.Count; i++)
+        {
+            if (!node.IsLeaf)
+                Traverse(node.Children[i], action);
+            action(node.Keys[i], node.Values[i]);
+        }
+        if (!node.IsLeaf)
+            Traverse(node.Children[i], action);
+    }
 }
