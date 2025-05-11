0159f23b93b216b743071f116aab32533d6e7988
Canka76 <karakoccan04@gmail.com>
2025-05-11 10:25:08 +0300

    added comment to hashtableHandler

--- src/Program.cs
+++ src/Program.cs
@@ -49,10 +49,10 @@ switch (builder.Configuration["Search:Index"])
             { "Dictionary", new Dictionary<int, HashSet<(string, int)>>(300) },
             { "SortedList", new SortedList<int, HashSet<(string, int)>>(300) },
             { "SortedDictionary", new SortedDictionary<int, HashSet<(string, int)>>() },
-            { "DoubleHashing", new DoubleHashingHashTable<int, HashSet<(string, int)>>(300) },
+            { "DoubleHashing", new DoubleHashingHashTable<int, HashSet<(string, int)>>(300000) },
             { "LinearProbing", new LinearProbingHashTable<int, HashSet<(string, int)>>(300000) },
             { "QuadraticProbing", new QuadraticProbingHashTable<int, HashSet<(string, int)>>(300000) },
-            { "SeparateChaining", new SeparateChainingHashTable<int, HashSet<(string, int)>>(300) },
+            { "SeparateChaining", new SeparateChainingHashTable<int, HashSet<(string, int)>>(300000) },
             { "AVL", new AVLTreeDictionary<int, HashSet<(string, int)>>() }
         };
         
@@ -150,6 +150,6 @@ SearchHandler.RegisterHandlers(app);
 // Register document handlers
 DocumentHandler.RegisterHandlers(app);
 
-HashTableHandler.RegisterHandlers(app);
+//HashTableHandler.RegisterHandlers(app);
 
 app.Run();
